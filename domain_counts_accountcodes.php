8d18a563980a693cabf15590b076333d92c414e3
Alex <40072887+alexdcrane@users.noreply.github.com>
2025-03-06 11:58:43 -0700

    Update domain_counts_accountcodes.php

--- domain_counts_accountcodes.php
+++ domain_counts_accountcodes.php
@@ -17,7 +17,7 @@
 
 	The Initial Developer of the Original Code is
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2023
+	Portions created by the Initial Developer are Copyright (C) 2008-2025
 	the Initial Developer. All Rights Reserved.
 
 	Contributor(s):
@@ -27,7 +27,7 @@
 //includes files
 	require_once dirname(__DIR__, 2) . "/resources/require.php";
 	require_once "resources/check_auth.php";
-	require_once "resources/paging.php";	
+	require_once "resources/paging.php";
 
 //include the class
 	require_once "resources/check_auth.php";
@@ -41,10 +41,13 @@
 		echo "access denied";
 		exit;
 	}
-	
+
 //add multi-lingual support
 	$language = new text;
 	$text = $language->get();
+
+//get the settings
+	$settings = new settings(['database' => $database, 'domain_uuid' => $_SESSION['domain_uuid'] ?? '', 'user_uuid' => $_SESSION['user_uuid'] ?? '']);
 
 //get the http values and set them as variables
 	$domain_uuid = check_str($_GET["id"]);
@@ -54,13 +57,13 @@
 //handle search term
 	$search = check_str($_GET["search"]);
 	if (strlen($search) > 0) {
-		$sql_mod = "and accountcode ILIKE '%".$search."%' "; 
+		$sql_mod = "and accountcode ILIKE '%".$search."%' ";
 	}
 	if (strlen($order_by) < 1) {
 		$order_by = "accountcode";
 		$order = "ASC";
 	}
-	
+
 //get total accountcode count from the database
 	$sql = "select count(DISTINCT accountcode) as num_rows from v_extensions where domain_uuid = '".$domain_uuid."' ".$sql_mod." ";
 	$database = new database;
@@ -76,7 +79,7 @@
 	list($paging_controls_mini, $rows_per_page, $var_3) = paging($total_accountcodes, $param, $rows_per_page, true); //top
 	list($paging_controls, $rows_per_page, $var_3) = paging($total_accountcodes, $param, $rows_per_page); //bottom
 	$offset = $rows_per_page * $_GET['page'];
-	
+
 //get the domain name from the database
 	$sql = "SELECT domain_name  \n";
 	$sql .= "FROM v_domains \n";
@@ -85,7 +88,7 @@
 	$result = $database->select($sql, null);
 	$domain_result = $database->result;
 	unset($database,$result);
-	
+
 //get all the accountcodes from the database
 	$sql = "SELECT accountcode, count(*) AS count  \n";
 	$sql .= "FROM v_extensions \n";
@@ -100,7 +103,7 @@
 
 //set the http header
 	if ($_REQUEST['type'] == "csv") {
-	
+
 		//set the headers
 			header('Content-type: application/octet-binary');
 			header("Content-Disposition: attachment; filename='".$domain_result[0]['domain_name']."_accountcodes_" . date("Y-m-d") . ".csv'");
@@ -117,7 +120,7 @@
 				$z++;
 			}
 			echo "\n";
-		
+
 		//add the values to the csv
 			$x = 0;
 			foreach($directory as $codes) {
@@ -136,53 +139,32 @@
 			}
 			exit;
 	}
-	
+
 //additional includes
 	require_once "resources/header.php";
 	$document['title'] = $text['title-domain_counts_accountcodes'];
 
-//set the alternating styles
-	$c = 0;
-	$row_style["0"] = "row_style0";
-	$row_style["1"] = "row_style1";
-
 //show the content
-	echo "<table width=\"100%\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">\n";
-	echo "  <tr>\n";
-	echo "	<td align='left' width='100%'>\n";
-	echo "		<b>".$text['header-domain_counts_accountcodes'].": ".$domain_result[0]['domain_name']." (".$numeric_accountcodes.")</b><br>\n";
-	echo "	</td>\n";
-	echo "		<td align='right' width='100%' style='vertical-align: top;'>";
+	echo "<div class='action_bar' id='action_bar'>\n";
+	echo "	<div class='heading'><b>".$text['header-domain_counts_accountcodes'].":</b> ".$domain_result[0]['domain_name']." <div class='count'>".number_format($numeric_accountcodes)."</div></div>\n";
+	echo "	<div class='actions'>\n";
 	echo "		<form method='get' action=''>\n";
-	echo "			<td style='vertical-align: top; text-align: right; white-space: nowrap;'>\n";
-	echo "				<input type='button' class='btn' alt='".$text['button-back']."' onclick=\"window.location='domain_counts.php'\" value='".$text['button-back']."'>\n";	
-	echo "				<input type='text' class='txt' style='width: 150px' name='search' id='search' value='".escape($search)."'>";
-	echo "				<input type='hidden' name='id' value='".escape($domain_uuid)."' />";	
-	echo "				<input type='submit' class='btn' name='submit' value='".$text['button-search']."'>";
-	echo "				<input type='button' class='btn' value='".$text['button-export']."' ";
-	echo "onclick=\"window.location='domain_counts_accountcodes.php?";
-	if (strlen($_SERVER["QUERY_STRING"]) > 0) { 
-		echo $_SERVER["QUERY_STRING"]."&type=csv';\">\n";
-	} else { 
-		echo "type=csv';\">\n";
-	}
-
+	echo button::create(['type'=>'button','label'=>$text['button-back'],'icon'=>$_SESSION['theme']['button_icon_back'],'id'=>'btn_back','style'=>'margin-right: 15px;','onclick'=>"window.location='domain_counts.php'"]);
+	echo button::create(['type'=>'button','label'=>$text['button-export'],'icon'=>$settings->get('theme', 'button_icon_export'),'style'=>'margin-right: 15px;','onclick'=>"window.location='domain_counts_accountcodes.php?".(!empty($_SERVER["QUERY_STRING"]) ? $_SERVER["QUERY_STRING"]."&type=csv';" : "type=csv';")]);
+	echo "			<input type='text' class='txt' style='width: 150px' name='search' id='search' value=\"".escape($search)."\" placeholder=\"".$text['label-search']."\" onkeydown=''>";
+	echo button::create(['label'=>$text['button-search'],'icon'=>$settings->get('theme', 'button_icon_search'),'type'=>'submit','id'=>'btn_search']);
+	echo "			<input type='hidden' name='id' value='".escape($domain_uuid)."' />";
 
 #	if ($paging_controls_mini != '') {
 #		echo 			"<span style='margin-left: 15px;'>".$paging_controls_mini."</span>\n";
 #	}
-	echo "			</td>\n";
-	echo "		</form>\n";	
-	echo "  </tr>\n";
-	
-	
-	echo "	<tr>\n";
-	echo "		<td colspan='2'>\n";
-	echo "			".$text['description-domain_counts_accountcodes']."\n";
-	echo "		</td>\n";
-	echo "	</tr>\n";
-	echo "</table>\n";
-	echo "<br />";
+	echo "		</form>\n";
+	echo "	</div>\n";
+	echo "	<div style='clear: both;'></div>\n";
+	echo "</div>\n";
+
+	echo $text['description-domain_counts_accountcodes']."\n";
+	echo "<br /><br />\n";
 
 	echo "<form name='frm' method='post' action='domain_counts_delete.php'>\n";
 	echo "<table class='tr_hover' width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
@@ -202,5 +184,5 @@
 	echo "</form>";
 
 //show the footer
-	require_once "resources/footer.php";	
+	require_once "resources/footer.php";
 ?>
